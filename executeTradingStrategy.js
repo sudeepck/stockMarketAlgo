3590d545736f8d93b66efee81d14f25fd0f5a91b
sudeepck <56756571+sudeepck@users.noreply.github.com>
2023-03-01 13:21:04 +0530

    Add files via upload
    final commits

---
+++ executeTradingStrategy.js
@@ -0,0 +1,85 @@
+executeTradingStrategy =(tradeMarketData,threshold,averagePriceValue,accountBalance,str,brokeragebuyFee,brokerageSellFee) =>
+{
+    str +=`Date &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; &emsp &nbsp; &nbsp;&nbsp; BuyOrSell &nbsp;&nbsp; &nbsp;&nbsp;  avgPriceValue &emsp; current price &emsp; thresholdValue &emsp;  accountBalance &emsp; accountValue  &emsp; profitOrLoss <br/>`;
+    let accountValue = accountBalance;
+    let unchangedinitialBalance = accountBalance;
+
+   let sellingPrice = 0 ,purchasePrice = 0 ,numShares = 0,buyed = 0;
+   let numberofpurshaced = 0,numberofSelled =    0
+    let previousBuyedValue = 0;
+    let profit = 0;
+
+  
+
+         for (const timestamp in tradeMarketData["Time Series (5min)"]) {
+
+            let High = tradeMarketData["Time Series (5min)"][timestamp]["2. high"]
+            let Low = tradeMarketData["Time Series (5min)"][timestamp]["3. low"]
+            let Date = timestamp
+
+               let currentPriceValue = (parseInt(High) + parseInt(Low))/2
+                let priceDifference = Math.abs(currentPriceValue - averagePriceValue);//129.5 -- 129.3 = 0.3
+
+                // console.log(priceDifference  +  " " + threshold)
+                    // if sell  else buy
+                    if (priceDifference > threshold && buyed == 1 && previousBuyedValue < currentPriceValue) {
+                        flag = 1;
+                        sellingPrice += currentPriceValue;
+                        let brokeageFeeForSell = Math.min(brokerageSellFee[1].amount ,(currentPriceValue * brokerageSellFee[0].percentage));
+                        brokeageFeeForSell=  brokeageFeeForSell.toFixed(1);
+
+                        
+                        accountBalance += currentPriceValue - brokeageFeeForSell
+                        accountValue = accountBalance - brokeageFeeForSell
+                        numShares++;
+                        buyed --;
+                        numberofSelled++;   
+                        profit = previousBuyedValue < currentPriceValue ? "profit" :"loss";
+
+                        str +=`${Date} &nbsp; &nbsp;  &nbsp; &nbsp; SELL &emsp;&emsp;&emsp;&emsp;&emsp;${[averagePriceValue]} &emsp;&emsp;&emsp;&emsp;&emsp;${[currentPriceValue.toFixed(2)]} &emsp;&emsp;&emsp;&emsp;&emsp; ${[threshold]} &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;  ${[accountBalance.toFixed(2)]} &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;${[accountValue.toFixed(2)]} &emsp; ${profit} <br/>`
+                        // return {[Date] : "it's time to Sell the stock "}
+                    } else if (priceDifference < threshold && accountBalance >= currentPriceValue && buyed == 0) {
+                        previousBuyedValue = currentPriceValue;//94
+
+
+                        let brokeageFeeForBuy = Math.min(brokeragebuyFee[1].amount,(currentPriceValue * brokeragebuyFee[0].percentage));
+                       brokeageFeeForBuy  =brokeageFeeForBuy.toFixed(1)
+
+                    
+                        accountBalance -= currentPriceValue -brokeageFeeForBuy
+                        accountValue = accountBalance + currentPriceValue -brokeageFeeForBuy
+
+                        numShares++;
+                        buyed++;
+                        numberofpurshaced++;
+                        flag =1;
+                          str +=`${Date} &nbsp; &nbsp;  &nbsp; &nbsp; BUY &emsp;&emsp;&emsp;&emsp;&emsp;${[averagePriceValue]} &emsp;&emsp;&emsp;&emsp;&emsp;${[currentPriceValue.toFixed(2)]} &emsp;&emsp;&emsp;&emsp;&emsp; ${[threshold]} &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; ${[accountBalance.toFixed(2)]} &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;${[accountValue.toFixed(2)]} <br/>`
+                    } else {
+                        flag = 0;
+                    }
+        }
+    
+        
+
+        let finalBalance = accountBalance;
+        console.log("numberofpurshaced = " + numberofpurshaced + "\t" + "numberofSelled ="  +numberofSelled )
+        console.log("initialBalance = " +  unchangedinitialBalance +"\t"+ "finalBalance = " +  finalBalance);
+
+        console.log(str);
+        console.log("*************")
+    return str;
+}
+
+
+module.exports = {executeTradingStrategy}
+
+
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,55 @@
+//https://docs.google.com/spreadsheets/d/10VTlKKOG5bxZZwF6uxvsQ1waOr5Xe7-sX80ALLEDbLs/edit#gid=0
+//https://docs.google.com/spreadsheets/d/10VTlKKOG5bxZZwF6uxvsQ1waOr5Xe7-sX80ALLEDbLs/edit#gid=0
+
+//https://github.com/prediqtiv/alpha-vantage-cookbook/blob/master/symbol-lists.md
+
+const express = require('express')
+const app = express()
+const path = require('path');
+const {executeTradingStrategy} = require('./executeTradingStrategy')
+const fs = require('fs');
+const request = require('request')
+
+app.get('/',(req,res)=>{
+    res.send("Trading Platform")
+})
+
+app.get('/trading',async(req,res)=>{
+    const { companyTiker, averagePrice,threshold}= req.query
+
+    let str = "";
+    let thresholdValue = parseFloat(threshold); 
+    let averagePriceValue = parseFloat(averagePrice) // example reference price 0.7 96.5  11947.4
+    let accountBalance = 1000; 
+      let brokerageBuyFee = [{percentage:0.03},{amount :20}]
+    let brokerageSellFee = [{percentage:0.03},{amount :20}]
+    let tradeMarketData;
+
+    let url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${companyTiker}&interval=5min&outputsize=full&apikey=DIYPNSKCANYW21CM&datatype=json`;    
+        request.get({
+            url: url,
+            json: true,
+            headers: {'User-Agent': 'request'}
+        }, (err, res, data) => {
+            if (err) {
+            console.log('Error:', err);
+            } else if (res.statusCode !== 200) {
+            console.log('Status:', res.statusCode);
+            } else {
+                tradeMarketData = data
+                // console.log(tradeMarketData["Time Series (5min)"])
+                str = executeTradingStrategy(tradeMarketData,thresholdValue,averagePriceValue,accountBalance,str,brokerageBuyFee,brokerageSellFee);
+                console.log(str);
+            }
+        })
+
+        setTimeout(() => {
+              console.log("**********");
+              console.log(str);
+            res.send(str);
+        }, 5000);
+})
+
+app.listen(3000,()=>{
+    console.log("Connected to server");
+})
